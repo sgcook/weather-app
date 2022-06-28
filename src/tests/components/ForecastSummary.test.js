c78af2ef3bda83e5f14018aee44794f0768ab047
Sara Cook <saracook087@gmail.com>
2022-06-28 18:29:42 +0100

    added getByTestId for icon

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -25,4 +25,22 @@ describe("Forecast Summary", () => {
 
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it("renders correct values for props", () => {
+    const { getByText, getByTestId } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(getByText("1111111")).toHaveClass("forecast-summary__date");
+    expect(getByText("Stub description")).toHaveClass(
+      "forecast-summary__description"
+    );
+    expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary__icon");
+    expect(getByText("22°C")).toHaveClass("forecast-summary__temperature");
+  });
 });
