9b5b453c073bb4c5c907de6344666b35a6625ec0
Sara Cook <saracook087@gmail.com>
2022-07-01 16:40:02 +0100

    all tests pass

--- src/tests/components/ForecastSummaries.test.js
+++ src/tests/components/ForecastSummaries.test.js
@@ -3,34 +3,37 @@ import { render } from "@testing-library/react";
 import ForecastSummaries from "../../components/ForecastSummaries";
 
 describe("Forecast Summaries", () => {
-  const validProps = [
-    {
-      date: 1111111,
-      description: "Stub description1",
-      icon: "600",
-      temperature: {
-        min: 12,
-        max: 22,
+  const validProps = {
+    onForecastSelect: () => {},
+    forecasts: [
+      {
+        date: 1111111,
+        description: "Stub description1",
+        icon: "600",
+        temperature: {
+          min: 12,
+          max: 22,
+        },
+        onSelect: () => {},
       },
-      onSelect: () => {},
-    },
-    {
-      date: 2222222,
-      description: "Stub description2",
-      icon: "800",
-      temperature: {
-        min: 13,
-        max: 24,
+      {
+        date: 2222222,
+        description: "Stub description2",
+        icon: "800",
+        temperature: {
+          min: 13,
+          max: 24,
+        },
+        onSelect: () => {},
       },
-      onSelect: () => {},
-    },
-  ];
+    ],
+  };
 
   it("renders correctly", () => {
     const { asFragment } = render(
       <ForecastSummaries
-        forecasts={validProps}
-        onForecastSelect={validProps.onSelect}
+        forecasts={validProps.forecasts}
+        onForecastSelect={validProps.onForecastSelect}
       />
     );
     expect(asFragment()).toMatchSnapshot();
@@ -39,8 +42,8 @@ describe("Forecast Summaries", () => {
   it("renders the correct number of ForecastSummary instances", () => {
     const { getAllByTestId } = render(
       <ForecastSummaries
-        forecasts={validProps}
-        onForecastSelect={validProps.onSelect}
+        forecasts={validProps.forecasts}
+        onForecastSelect={validProps.onForecastSelect}
       />
     );
 
