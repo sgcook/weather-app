4c2777a1d0f1977aab651663bd084b0d738c266f
Sara Cook <saracook087@gmail.com>
2022-07-29 11:54:23 +0100

    removed console logs

--- src/requests/getForecast.js
+++ src/requests/getForecast.js
@@ -1,4 +1,3 @@
-/* eslint-disable-next-line no-console */
 import axios from "axios";
 
 const getForecast = (
@@ -25,11 +24,9 @@ const getForecast = (
       const { status } = error.response;
       if (status === 404) {
         setErrorMessage("No such town or city, try again!");
-        console.error("Location is not valid", error);
       }
       if (status === 500) {
         setErrorMessage("Oops, server error, try again later.");
-        console.error("Server error", error);
       }
     });
 };
